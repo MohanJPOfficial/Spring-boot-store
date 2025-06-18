5d0ae050cb0ae2a135a34f57ac7801a2045f275c
“MohanJP” <jpmohan007@gmail.com>
2025-06-18 22:34:19 +0530

    feat(7.11): Cleanup

--- src/main/java/com/mohanjp/store/StoreApplication.java
+++ src/main/java/com/mohanjp/store/StoreApplication.java
@@ -1,30 +1,14 @@
 package com.mohanjp.store;
 
-import com.mohanjp.store.data.entity.AddressEntity;
-import com.mohanjp.store.data.entity.ProfileEntity;
-import com.mohanjp.store.data.entity.UserEntity;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 
-		//ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
-		var user = UserEntity.builder()
-				.name("Mohan")
-				.password("22the333")
-				.email("jpmohan@gmail.com")
-				.build();
-
-		var profile = ProfileEntity.builder()
-						.bio("Software Engineer with a passion for coding and technology.")
-						.build();
-
-		user.setProfile(profile);
-		profile.setUser(user);
-
-		System.out.println("User: " + user);
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 	}
 }
