37aeed57aabbb7a9ded89d6bb900a47c9bee7552
MohanJP <jpmohan007@gmail.com>
2025-06-27 00:02:12 +0530

    feat(2.2): Creating dynamic route with HTTP status codes

--- src/main/java/com/mohanjp/store/controllers/UserController.java
+++ src/main/java/com/mohanjp/store/controllers/UserController.java
@@ -3,17 +3,32 @@ package com.mohanjp.store.controllers;
 import com.mohanjp.store.entity.UserEntity;
 import com.mohanjp.store.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<UserEntity> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable long id) {
+        var user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
     }
 }
