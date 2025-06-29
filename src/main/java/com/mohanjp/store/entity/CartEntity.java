2d18aa5a33d32ccae523a8231d67fca037164abb
MohanJP <jpmohan007@gmail.com>
2025-06-29 16:35:43 +0530

    feat(4.1): Creating entities

---
+++ src/main/java/com/mohanjp/store/entity/CartEntity.java
@@ -0,0 +1,28 @@
+package com.mohanjp.store.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class CartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "date_created", insertable = false, updatable = false)
+    private LocalDate dateCreated;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItemEntity> cartItems = new LinkedHashSet<>();
+}
