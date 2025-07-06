08f1d4c3620b95b9f6ab9ed280b895114be35839
“MohanJP” <jpmohan007@gmail.com>
2025-07-06 19:35:49 +0530

    feat(4.5): Refactoring towards an object-oriented design

--- src/main/java/com/mohanjp/store/entity/CartEntity.java
+++ src/main/java/com/mohanjp/store/entity/CartEntity.java
@@ -32,4 +32,28 @@ public class CartEntity {
                 .map(CartItemEntity::getTotalPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
+    public CartItemEntity getItem(Long productId) {
+        return items.stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public CartItemEntity addItem(ProductEntity product) {
+
+        var cartItem = getItem(product.getId());
+
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        } else {
+            cartItem = new CartItemEntity();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(this);
+            items.add(cartItem);
+        }
+
+        return cartItem;
+    }
 }
