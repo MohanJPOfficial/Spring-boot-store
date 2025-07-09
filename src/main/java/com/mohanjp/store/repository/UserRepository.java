c279c65bcff5a54723ef440fa95e3920c3a971cd
MohanJP <jpmohan007@gmail.com>
2025-07-10 00:02:47 +0530

    feat(5.4): Exercise: Building the login API

--- src/main/java/com/mohanjp/store/repository/UserRepository.java
+++ src/main/java/com/mohanjp/store/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.mohanjp.store.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByEmail(String email);
 }
