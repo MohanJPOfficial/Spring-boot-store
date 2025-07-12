1920be41293391f8c55902a32b276e83a8c2689d
MohanJP <jpmohan007@gmail.com>
2025-07-12 23:20:54 +0530

    feat(5.10): Implementing a custom filter to validate JWTs

--- src/main/java/com/mohanjp/store/config/SecurityConfig.java
+++ src/main/java/com/mohanjp/store/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mohanjp.store.config;
 
+import com.mohanjp.store.filter.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @AllArgsConstructor
@@ -46,7 +48,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
 
         http.sessionManagement(c ->
                         c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -57,7 +59,8 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/validate").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
