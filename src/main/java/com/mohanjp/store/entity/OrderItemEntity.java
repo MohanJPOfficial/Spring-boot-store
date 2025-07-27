76deb4587694c4aeb7625ba25e4326f437d8a83c
“MohanJP” <jpmohan007@gmail.com>
2025-07-27 17:31:59 +0530

    feat(6.2): Creating order entities

---
+++ src/main/java/com/mohanjp/store/entity/OrderItemEntity.java
@@ -0,0 +1,31 @@
+package com.mohanjp.store.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_items", schema = "store")
+public class OrderItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+}
---
+++ src/main/java/com/mohanjp/store/entity/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.mohanjp.store.entity;
+
+public enum OrderStatus {
+    PENDING,
+    PAID,
+    FAILED,
+    CANCELLED
+}
