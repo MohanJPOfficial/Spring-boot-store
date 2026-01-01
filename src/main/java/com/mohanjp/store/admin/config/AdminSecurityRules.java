5c971f8c95cf4aa2ec886417a720378ee47e6c7c
“MohanJP” <jpmohan007@gmail.com>
2026-01-01 19:19:59 +0530

    feat(8.5): Modularize the security rules by feature

---
+++ src/main/java/com/mohanjp/store/admin/config/AdminSecurityRules.java
@@ -0,0 +1,16 @@
+package com.mohanjp.store.admin.config;
+
+import com.mohanjp.store.common.config.SecurityRules;
+import com.mohanjp.store.user.entity.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSecurityRules implements SecurityRules {
+
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/admin/**").hasRole(Role.ADMIN.name());
+    }
+}
---
+++ src/main/java/com/mohanjp/store/product/config/ProductSecurityRules.java
@@ -0,0 +1,20 @@
+package com.mohanjp.store.product.config;
+
+import com.mohanjp.store.common.config.SecurityRules;
+import com.mohanjp.store.user.entity.Role;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductSecurityRules implements SecurityRules {
+
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers(HttpMethod.GET, "/products/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/products/**").hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, "/products/**").hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole(Role.ADMIN.name());
+    }
+}
